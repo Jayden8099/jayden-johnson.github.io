4405ee0af83542c415f3c74f3d9c2c81c79845d5
Jayden8099 <2203021@spsd.sk.ca>
2024-09-10 15:18:36 -0600

    First Project

---
+++ 00 First p5 project/sketch.js
@@ -0,0 +1,17 @@
+// Project Title
+// Your Name
+// Date
+//one-line description (more if needed)
+
+
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  //background(220);
+  circle(mouseX,mouseY,30)
+}
