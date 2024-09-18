e67ccc669a9dbbad432a83b5c05500b2678761a4
Jayden8099 <2203021@spsd.sk.ca>
2024-09-17 19:56:16 -0600

    Updates
    cleaned up code
    
    added a cloud controlled with wasd
    
    added name in bottom right
    
    modified various values

--- 05 Interactive Scene/sketch.js
+++ 05 Interactive Scene/sketch.js
@@ -6,18 +6,23 @@
 let shapeSize = 50;
 let currentBack = 0;
 
-//cloud x and y pos
+//cloud x position
 let cX = 200
-ran1 = Math.floor(Math.random() * 950);
-ran2 = Math.floor(Math.random() * 950);
-ran3 = Math.floor(Math.random() * 950);
-ran4 = Math.floor(Math.random() * 950);
+//generates random y coordinates for the clouds
+let cY1 = Math.floor(Math.random() * 800);
+let cY2 = Math.floor(Math.random() * 800);
+let cY3 = Math.floor(Math.random() * 800);
+let cY4 = Math.floor(Math.random() * 800);
 
 
+//cloud character x and y
+let ccX = 100
+let ccY = 100
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
     
 }
 
@@ -27,33 +32,39 @@ function draw() {
   sun();
   rectMode(CENTER);
   cloud(cX);
+  cloudCharacter();
   
-  cX += 0.5
-  if(cX >= width) {
+  cX += cY1 * 0.01
+  if(cX >= width + 270) {
+    cloudY();
     cX = -270;
+    
   }
-  
-}
-  
+
+textSize(50)
+text("Jayden", width-200, height-20); 
+}  
   
 
 
 function sun(){
-  //draws the moving sun in the top
+  //draws the moving sun in the top that follows the mouse
   fill(255,255,0);
   noStroke();
-  circle(mouseX,height/5,shapeSize+20);
+  circle(mouseX,mouseY,shapeSize+40);
   fill(0,0,0);
-  circle(mouseX-15,height/5,shapeSize-40);
-  circle(mouseX+15,height/5,shapeSize-40);
-  rect(mouseX,height/5-30,shapeSize+5,6);
-  rect(mouseX,height/5-55,shapeSize-20,45);
+  circle(mouseX-15,mouseY-10,shapeSize-38);
+  circle(mouseX+15,mouseY-10,shapeSize-38);
+  rect(mouseX,mouseY-37,shapeSize+7,10);
+  rect(mouseX,mouseY-60,shapeSize-20,45);
+
 }
 
 
 function earth(){
 //draws the earth
-  fill(0,100,255);
+
+fill(0,100,255);
 circle(width/2,height,1200);
 fill(0,255,0);
 circle(width/2+300,height-150,+150);
@@ -62,6 +73,8 @@ circle(width/2,height-100,shapeSize);
 fill(0,100,255)
 circle(width/2,height-100,shapeSize-15);
 }
+
+
 
 function mousePressed() {
 //changes currentBack by one so the background changes
@@ -76,40 +89,62 @@ function backDrop(){
   //uses currentBack to determine the background color
   if(currentBack === 0) background(0,200,255);
   
-  else if(currentBack === 1) background(150,200,0);
+  else if(currentBack === 1) background(220,150,0);
 
-  else if(currentBack === 2) background(183,0,200);
+  else if(currentBack === 2) background(100,0,200);
 
-  else background(123,57,100);
+  else background(20,20,20);
 }
 
 
 function cloud(cX){
-
-
-
-
-
+  //draws 4 clouds at a random y coord (0-800)
+  //tried to get a loop working but could not find a way
   
   fill(255)
-  ellipse(cX, ran1, 80, 60);
-  ellipse(cX + 10, ran1  + 10, 80, 60);
-  ellipse(cX - 20, ran1 + 10, 80, 60);
+  ellipse(cX, cY1, 110, 90);
+  ellipse(cX + 20, cY1  + 20, 110, 90);
+  ellipse(cX - 20, cY1 + 20, 110, 90);
+
+  
+  ellipse(cX+ 75, cY2, 80, 60);
+  ellipse(cX + 75 + 10, cY2  + 10, 80, 60);
+  ellipse(cX + 75 - 20, cY2 + 10, 80, 60);
 
 
+  ellipse(cX - 100, cY3, 110, 90);
+  ellipse(cX - 100 + 10, cY3  + 10, 110, 90);
+  ellipse(cX - 100 - 20, cY3  + 10, 110, 90);
 
-  ellipse(cX+ 75, ran2, 80, 60);
-  ellipse(cX + 75 + 10, ran2  + 10, 80, 60);
-  ellipse(cX + 75 - 20, ran2 + 10, 80, 60);
+  ellipse(cX + 250, cY4, 80, 60);
+  ellipse(cX + 250  + 10, cY4  + 10, 80, 60);
+  ellipse(cX + 250 - 20, cY4 + 10, 80, 60);
+}
 
 
-  ellipse(cX - 100, ran3, 80, 60);
-  ellipse(cX - 100 + 10, ran3  + 10, 80, 60);
-  ellipse(cX - 100 - 20, ran3 + 10, 80, 60);
+function cloudY(){
+  //when the clouds reach the end of the screen it randomizes the y pos
+  cY1 = Math.floor(Math.random() * 800);
+  cY2 = Math.floor(Math.random() * 800);
+  cY3 = Math.floor(Math.random() * 800);
+  cY4 = Math.floor(Math.random() * 800);
+}
 
-  ellipse(cX + 250, ran4, 80, 60);
-  ellipse(cX + 250  + 10, ran4  + 10, 80, 60);
-  ellipse(cX + 250 - 20, ran4 + 10, 80, 60);
 
- 
+function cloudCharacter() {
+  fill(255)
+  ellipse(ccX, ccY, 80, 60);
+  ellipse(ccX + 10, ccY  + 10, 80, 60);
+  ellipse(ccX - 20, ccY + 10, 80, 60);
+  fill(0,0,0)
+  circle(ccX+15,ccY,shapeSize-40)
+  circle(ccX-15,ccY,shapeSize-40)
+  
+
+  
+  //uses WASD to controll the cloud character(the one with eyes)
+  if(keyIsPressed && key === "a") ccX -= 5;
+  else if(keyIsPressed && key === "d") ccX += 5;
+  else if(keyIsPressed && key === "s") ccY += 5;
+  else if(keyIsPressed && key === "w") ccY -= 5;
 }
