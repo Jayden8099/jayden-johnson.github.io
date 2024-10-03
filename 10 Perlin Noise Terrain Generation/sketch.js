d773fe5afe8c950ed453ddf43aac6bc0ce8cd952
Jayden8099 <2203021@spsd.sk.ca>
2024-10-02 22:59:00 -0600

    Updates to the Terrain generation
    -Added Flag at the highest peak

--- 10 Perlin Noise Terrain Generation/sketch.js
+++ 10 Perlin Noise Terrain Generation/sketch.js
@@ -5,78 +5,108 @@
 
 
 
+//The rectangles width
+let rectW = 1;
 
-let rectW = 3;
+
+let rTime = 5;
+let rInterval = 0.01
 
 
+//gets the flag x and y based on the higest point
 let flagX = 0;
-let flagY = 1;
+let flagY = 0;
 
 
 
 
 function setup() {
+ 
   createCanvas(windowWidth, windowHeight);
-  background(220);
-  generateTerrain();
-  drawFlag(width / 2, flagY);
-  print(flagY);
+  //background(220);
+  //generateTerrain();
+
+
 
 }
 
 function draw() {
-  
-  
+  rTime = 5;
+  background(220);
+  generateTerrain();
+  drawFlag(flagX, height - flagY);
+
+
+
 
 }
 
 
 
 function generateTerrain() {
-
+  //randomly generates the terrain
+  
   fill(255);
-  let cTime = 1;
 
+  flagX = 0
+  flagY = 0
 
   for (let x = 0; x <= width; x += rectW) {
-
-    let rectHeight = noise(cTime);
-    rectHeight = map(rectHeight, 0, 1, 10, 700);
+    //noiseSeed(5);
+    let rectHeight = noise(rTime);
+    rectHeight = map(rectHeight, 0, 1, 1, 800);
 
     rect(x, height, rectW, -rectHeight);
-    cTime += 0.01
+    rTime += rInterval;
+    if(x >= width){
+      x - rectW;
+    }
     
 
-    
-    if (flagY < rectHeight) {
-      flagY = rectHeight
-      print("RECT " + rectHeight);
+
+
+
+
+    //coords for the highest point
+    if (flagY <= rectHeight) {
+      flagY = rectHeight;
+      flagX = x;
     }
   }
-  
+
 }
 
 function keyPressed() {
-  background(220)
 
 
+  //if the right arrow is pressed it widens the rectangles
+  //redraws the flag at the highest peak
   if (keyCode === RIGHT_ARROW) {
     background(220);
+    rectW += 0.5;
     generateTerrain();
-    rectW += 0.3
+
+
+    //if the left arrow is pressed it slims the rectangles
+    //redraws the flag at the highest peak
   } else if (keyCode === LEFT_ARROW) {
     background(220);
+    rectW -= 0.5;
     generateTerrain();
-    rectW -= 0.3
+
+
+
     if (rectW <= 1) {
-      rectW = 1
+      rectW = 1;
     }
   }
 }
 
 function drawFlag(x, y) {
-  fill(255, 255, 6);
-  rect(x, y, 20);
+  //draws the flag at the highest peak
+  fill(255, 255, 0);
+  rect(x, y, 0, -20);
+  triangle(x, y - 20, x, y - 10, x + 10, y - 15);
 }
 
 
