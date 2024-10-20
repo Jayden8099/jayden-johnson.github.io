9841d8c1d6b3ef2dee68fbb0c80c8eb16d9ad7f8
Jayden8099 <2203021@spsd.sk.ca>
2024-10-20 16:04:35 -0600

    Updated Cars Cars Cars

--- 21 Cars Cars Cars/sketch.js
+++ 21 Cars Cars Cars/sketch.js
@@ -6,29 +6,30 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
 }
 
 function draw() {
   background(220);
-
   drawRoad();
+  
 }
 
 
 function drawRoad() {
-
+  // Draws the Road
   fill(0);
-  rect(0, height / 2 - 100, width, 200);
+  rect(0, height / 2 - 250, width, 500);
 
-  
-  
-  //FIX
-  
-  // for (let x = 0; x <= width; x + 20) {
-  //   stroke('white');
-  //   strokeWeight(2);
-  //   line(0 + x, height / 2, 0 + x * 2, height / 2);
-  // }
+  //Draws the White Dividing Lines
+  for (let i = 0; i < width; i += 20) {
+    stroke('white');
+    strokeWeight(5);
+    line(i, height / 2, i += 20, height / 2);
+  }
+
+
+
 
 
 
@@ -37,6 +38,47 @@ function drawRoad() {
 
 class Vehicle {
   constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.type = random(0, 1);
+    this.color = color(random(255), random(255), random(255));;
+    this.direction = random(0, 1);
+    this.xSpeed = 5;
+    this.MAX_SPEED = 15;
+  }
+  display() {
+    if (this.type === 0) {
+      fill(this.color);
+      rect(this.x, this.y, 100, 50);
+    }
+    else {
+      fill(this.color);
+      rect(this.x, this.y, 100);
+    }
+  }
 
+  move() {
+    if (this.direction === 0) {
+      this.x += this.xSpeed;
+    }
+    else {
+      this.x += -this.xSpeed
+    }
+  }
+
+
+  speedUp() {
+    if (this.direction === 0) {
+      this.xSpeed + 1;
+      if (this.xSpeed >= this.MAX_SPEED) {
+        this.xSpeed = this.MAX_SPEED;
+      }
+    }
+    else {
+      -this.xSpeed - 1;
+      if (-this.xSpeed <= -this.MAX_SPEED) {
+        this.xSpeed = -this.MAX_SPEED;
+      }
+    }
   }
 }
