928a37b79a111dc673178a292413b391a753ca2c
Jayden8099 <2203021@spsd.sk.ca>
2024-10-23 15:18:51 -0600

    Started traffic light

--- 21 Cars Cars Cars/sketch.js
+++ 21 Cars Cars Cars/sketch.js
@@ -6,21 +6,24 @@
 let eastbound = [];
 let westbound = [];
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
   //Pushes 20 new Vehicles to each Array at a random X and Y based on direction
   //Aswell as a random vehicle type
   for (let i = 0; i < 20; i++) {
-    eastbound.push(new Vehicle((random(0, width)), random(height / 2 + 30, height / 2 + 200), round(random(0, 1)), 0));
-    westbound.push(new Vehicle((random(0, width)), random(height / 2 - 30, height / 2 - 200), round(random(0, 1)), 1));
+    eastbound.push(new Vehicle(random(0, width), random(height / 2 + 30, height / 2 + 200), round(random(0, 1)), 0));
+    westbound.push(new Vehicle(random(0, width), random(height / 2 - 30, height / 2 - 200), round(random(0, 1)), 1));
   }
+  trafficL = new trafficLight(0);
 }
 
 function draw() {
+
   background(220);
   drawRoad();
-
+  trafficL.display();
 
   for (let i = 0; i < eastbound.length; i++) {
     eastbound[i].action();
@@ -28,22 +31,24 @@ function draw() {
   for (let i = 0; i < westbound.length; i++) {
     westbound[i].action();
   }
-
-
-
 }
 function mouseClicked() {
   //If shift left clicking  push a new Vehicle to westbound
   if (keyIsPressed && keyCode === SHIFT) {
-    westbound.push(new Vehicle((random(0, width)), random(height / 2 - 30, height / 2 - 200), round(random(0, 1)), 1));
-    
+    westbound.push(new Vehicle(random(0, width), random(height / 2 - 30, height / 2 - 200), round(random(0, 1)), 1));
+
   }
   //If Left clicking push a new vehicle to eastbound
-  else{
-    eastbound.push(new Vehicle((random(0, width)), random(height / 2 + 30, height / 2 + 200), round(random(0, 1)), 0));
-    
+  else {
+    eastbound.push(new Vehicle(random(0, width), random(height / 2 + 30, height / 2 + 200), round(random(0, 1)), 0));
   }
 }
+function keyPressed() {
+  if (keyCode === 32) {
+    trafficL.update();
+  }
+}
+
 
 
 function drawRoad() {
@@ -158,8 +163,42 @@ class Vehicle {
     }
     else if (this.rng === 3) {
       this.color = color(random(255), random(255), random(255));
-
     }
-
   }
 }
+class trafficLight {
+  constructor(state) {
+    this.state = state;
+    this.interval = 2000;
+    this.prevTime;
+    this.check;
+  }
+  display() {
+    fill(0);
+    rect(80, 80, 100, 200);
+    if (this.state === 0) {
+      fill(0, 255, 0);
+      circle(80, 50, 60);
+    }
+    else {
+      fill(255, 0, 0);
+      circle(80, 120, 60);
+    }
+  }
+  update() {
+    this.prevTime = millis();
+    
+    this.state = 1;
+    
+    this.check = this.prevTime + this.interval;
+
+    print(this.state);
+    if (millis() > this.check) {
+      this.state = 0;
+      print(this.state);
+    }
+  }
+
+}
+
+
